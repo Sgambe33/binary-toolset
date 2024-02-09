b81c141b369ec313e356c2d202c8b9887542ee84
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-02-09 16:04:00 +0100

    Changed syntax to use class methods
    Signed-off-by: Sgambe33 <80386324+Sgambe33@users.noreply.github.com>

--- lib/pages/converter.dart
+++ lib/pages/converter.dart
@@ -1,3 +1,4 @@
+import 'package:binarytoolset/converters.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:math';
@@ -6,7 +7,7 @@ class Converter extends StatefulWidget {
   const Converter({Key? key}) : super(key: key);
 
   @override
-  _ConverterState createState() => _ConverterState();
+  State<Converter> createState() => _ConverterState();
 }
 
 class _ConverterState extends State<Converter> {
@@ -15,204 +16,122 @@ class _ConverterState extends State<Converter> {
   final TextEditingController _hexController = TextEditingController();
   final TextEditingController _octalController = TextEditingController();
 
-  String decimalToBinary(int n) {
-    String binary = '';
-    int spaces = 0;
-    do {
-      // Every 5 digits, add a space, but the number of digits must always be a multiple of 5
-      if (binary.isNotEmpty && (binary.length - spaces) % 4 == 0) {
-        binary = ' $binary';
-        spaces++;
-      }
-      binary = (n % 2).toString() + binary;
-      n = n ~/ 2;
-    } while (n > 0);
-
-    // Pad with leading zeros to make the length a multiple of 5
-    int remainder = (binary.length-spaces) % 4;
-    if (remainder > 0) {
-      binary = '0' * (4 - remainder) + binary;
-    }
-
-    return binary;
-  }
-
-  String decimalToOctal(int n) {
-    String octal = '';
-    while (n > 0) {
-      octal = (n % 8).toString() + octal;
-      n = n ~/ 8;
-    }
-    return octal;
-  }
-
-  String decimalToHex(int n) {
-    String hex = '';
-    while (n > 0) {
-      int rem = n % 16;
-      if (rem < 10) {
-        hex = rem.toString() + hex;
-      } else {
-        hex = String.fromCharCode(rem + 55) + hex;
-      }
-      n = n ~/ 16;
-    }
-    return hex;
-  }
-
-  int binaryToDecimal(String n) {
-    int decimal = 0;
-    int exp = 0;
-    String reversed = n.split('').reversed.join();
-    for (int i = 0; i < reversed.length; i++) {
-      decimal += int.parse(reversed[i]) * pow(2, exp) as int;
-      exp++;
-    }
-    return decimal;
-  }
-
-  int octalToDecimal(String n) {
-    int decimal = 0;
-    int exp = 0;
-    String reversed = n.split('').reversed.join();
-    for (int i = 0; i < reversed.length; i++) {
-      decimal += int.parse(reversed[i]) * pow(8, exp) as int;
-      exp++;
-    }
-    return decimal;
-  }
-
-  int hexToDecimal(String n) {
-    int decimal = 0;
-    int exp = 0;
-    for (int i = n.length - 1; i > 0; i--) {
-      if (n[i] == 'A' ||
-          n[i] == 'B' ||
-          n[i] == 'C' ||
-          n[i] == 'D' ||
-          n[i] == 'E' ||
-          n[i] == 'F') {
-        decimal += (n.codeUnitAt(i) - 55) * pow(16, exp) as int;
-      } else {
-        decimal += int.parse(n[i]) * pow(16, exp) as int;
-      }
-      exp++;
-    }
-    return decimal;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Base Converter'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextField(
-                controller: _binaryController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Binary',
-                  hintText: 'Enter a binary number',
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _binaryController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Binary',
+                    hintText: 'Enter a binary number',
+                  ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-1]')),
+                  ],
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      _decimalController.text = Converters.binaryToDecimal(value).toString();
+                      _octalController.text =
+                          Converters.decimalToOctal(Converters.binaryToDecimal(value));
+                      _hexController.text = Converters.decimalToHex(Converters.binaryToDecimal(value));
+                    } else {
+                      _binaryController.text = '';
+                      _octalController.text = '';
+                      _hexController.text = '';
+                      _decimalController.text = '';
+                    }
+                  },
                 ),
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp(r'[0-1]')),
-                ],
-                onChanged: (value) {
-                  if (value.isNotEmpty) {
-                    _decimalController.text = binaryToDecimal(value).toString();
-                    _octalController.text =
-                        decimalToOctal(binaryToDecimal(value));
-                    _hexController.text = decimalToHex(binaryToDecimal(value));
-                  } else {
-                    _binaryController.text = '';
-                    _octalController.text = '';
-                    _hexController.text = '';
-                  }
-                },
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: _octalController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Octal',
-                  hintText: 'Enter an octal number',
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _octalController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Octal',
+                    hintText: 'Enter an octal number',
+                  ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-7]')),
+                  ],
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      _decimalController.text = Converters.octalToDecimal(value).toString();
+                      _binaryController.text =
+                          Converters.decimalToBinary(Converters.octalToDecimal(value));
+                      _hexController.text = Converters.decimalToHex(Converters.octalToDecimal(value));
+                    } else {
+                      _binaryController.text = '';
+                      _octalController.text = '';
+                      _hexController.text = '';
+                      _decimalController.text = '';
+                    }
+                  },
                 ),
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp(r'[0-7]')),
-                ],
-                onChanged: (value) {
-                  if (value.isNotEmpty) {
-                    _decimalController.text = octalToDecimal(value).toString();
-                    _binaryController.text =
-                        decimalToBinary(octalToDecimal(value));
-                    _hexController.text = decimalToHex(octalToDecimal(value));
-                  } else {
-                    _binaryController.text = '';
-                    _octalController.text = '';
-                    _hexController.text = '';
-                  }
-                },
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: _decimalController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Decimal',
-                  hintText: 'Enter a decimal number',
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _decimalController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Decimal',
+                    hintText: 'Enter a decimal number',
+                  ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                  ],
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      _binaryController.text = Converters.decimalToBinary(int.parse(value));
+                      _octalController.text = Converters.decimalToOctal(int.parse(value));
+                      _hexController.text = Converters.decimalToHex(int.parse(value));
+                    } else {
+                      _binaryController.text = '';
+                      _octalController.text = '';
+                      _hexController.text = '';
+                      _decimalController.text = '';
+                    }
+                  },
                 ),
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-                ],
-                onChanged: (value) {
-                  if (value.isNotEmpty) {
-                    _binaryController.text = decimalToBinary(int.parse(value));
-                    _octalController.text = decimalToOctal(int.parse(value));
-                    _hexController.text = decimalToHex(int.parse(value));
-                  } else {
-                    _binaryController.text = '';
-                    _octalController.text = '';
-                    _hexController.text = '';
-                  }
-                },
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: _hexController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Hexadecimal',
-                  hintText: 'Enter an hexadecimal number',
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _hexController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Hexadecimal',
+                    hintText: 'Enter an hexadecimal number',
+                  ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9a-fA-F]')),
+                  ],
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      _decimalController.text = Converters.hexToDecimal(value).toString();
+                      _binaryController.text = Converters.decimalToBinary(int.parse(_decimalController.text));
+                      _octalController.text = Converters.decimalToOctal(int.parse(_decimalController.text));
+                    } else {
+                      _binaryController.text = '';
+                      _octalController.text = '';
+                      _hexController.text = '';
+                      _decimalController.text = '';
+                    }
+                  },
                 ),
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp(r'[0-9a-fA-F]')),
-                ],
-                onChanged: (value) {
-                  if (value.isNotEmpty) {
-                    _decimalController.text =
-                        binaryToDecimal(decimalToBinary(int.parse(value)))
-                            .toString();
-                    _binaryController.text = decimalToBinary(
-                        binaryToDecimal(decimalToBinary(int.parse(value))));
-                    _octalController.text = decimalToOctal(
-                        binaryToDecimal(decimalToBinary(int.parse(value))));
-                  } else {
-                    _binaryController.text = '';
-                    _octalController.text = '';
-                    _hexController.text = '';
-                  }
-                },
               ),
             ],
           ),
