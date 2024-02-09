7f2d523f9e3b84a86cc0e203b6ec85c5569a1ea2
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-02-09 16:03:03 +0100

    Added new pages: calculator, ieee754, signed numbers
    Signed-off-by: Sgambe33 <80386324+Sgambe33@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:binarytoolset/pages/calculator.dart';
+import 'package:binarytoolset/pages/ieee754.dart';
+import 'package:binarytoolset/pages/signed_numbers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'firebase_options.dart';
 import 'pages/converter.dart';
 
@@ -17,14 +22,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData.dark(useMaterial3: true),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       routes: {
         '/converter': (context) => const Converter(),
-        '/main': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+        '/calculator': (context) => const Calculator(),
+        '/ieee754': (context) => const Ieee754(),
+        '/signed_numbers': (context) => const SignedNumbers(),
+        '/main': (context) => const MyHomePage(title: 'Binary Toolset'),
       },
       initialRoute: '/main',
     );
@@ -64,6 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text('Menu'),
             ),
             ListTile(
+              leading: const Icon(Icons.swap_horiz),
               title: const Text('Converter'),
               onTap: () {
                 Navigator.pop(context);
@@ -71,6 +77,46 @@ class _MyHomePageState extends State<MyHomePage> {
                   context,
                   MaterialPageRoute(builder: (context) => const Converter()),
                 );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.calculate),
+              title: const Text('Calculator'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Calculator()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.memory),
+              title: const Text('IEEE 754'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Ieee754()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.exposure),
+              title: const Text('Signed Numbers'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignedNumbers()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('About'),
+              onTap: () async {
+                await launchUrlString("https://github.com/Sgambe33/binary-toolset");
               },
             ),
           ],
